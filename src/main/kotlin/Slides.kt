c3312e9c8fe9a46a426d7c637ee344cd2a169651
pambrose <pambrose@mac.com>
2021-11-23 17:33:14 -0800

    Update content

--- src/main/kotlin/Slides.kt
+++ src/main/kotlin/Slides.kt
@@ -8,22 +8,18 @@ val slides =
     slide("Season to Leave") {
       addText(
         """
-            <h1>Welcome to this lesson</h1>
-            ## I am a header 
-            ### I am an even smaller header 
-            
-            Here are some bullet points:
-            * Item 1 with *italics*
-            * Item 2 with **bold**
-            
-            When will you choose to leave?
-        """
-      )
-
-      addImage(
-        "https://www.nps.gov/articles/000/images/Runaway-Slave-Advertisement-1_Columbus-Democrat-Columbus-MS-_18-August-1838_2.jpg",
-        300,
-        300
+          <h1>Welcome to this lesson</h1>
+          ## I am a header 
+          ### I am an even smaller header 
+          
+          Here are some bullet points:
+          * Item 1 with *italics*
+          * Item 2 with **bold**
+          
+          When will you choose to leave?
+   
+         <img src="https://www.nps.gov/articles/000/images/Runaway-Slave-Advertisement-1_Columbus-Democrat-Columbus-MS-_18-August-1838_2.jpg" alt="Pic" width="300" height="400" style="border:5px solid black"/>
+     """
       )
 
       verticalChoices()
